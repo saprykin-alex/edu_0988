3fee1a9a59a02f616a656d435f335676da5ac51c
saprykin-alex <88132212+saprykin-alex@users.noreply.github.com>
2021-07-29 12:34:59 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,7 @@
+public  class Main  {
+    public static void main(String[] args) {
+        int number = 255;
+        System.out.println(number * number);
+        //Квадрат числа 255 равен 65025
+    }
+}
